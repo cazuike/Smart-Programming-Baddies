3fee22af66cd7d933089d7a91cb98709eccc7de8
cazuike <chidi15azuike@gmail.com>
2024-10-08 10:29:20 -0400

    initial setup of service

---
+++ serviceSPB/src/main/java/com/smartprogrammingbaddies/App.java
@@ -0,0 +1,13 @@
+package com.smartprogrammingbaddies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
