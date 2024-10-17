a74a020d31aa6bb5b35318687b05adf7f94687d2
Bryan Granda <bryan.a.granda@gmail.com>
2024-10-16 21:37:30 -0400

    Chore: Fixed stylechecking errors given in the 3 classes.

--- serviceSPB/src/test/java/com/smartprogrammingbaddies/AppTest.java
+++ serviceSPB/src/test/java/com/smartprogrammingbaddies/AppTest.java
@@ -1,18 +1,18 @@
 package com.smartprogrammingbaddies;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void testApp() {
-        assertTrue(true);
-    }
+  /**
+    * Assert that the App is running.
+    */
+  @Test
+  public void testApp() {
+    assertTrue(true);
+  }
 }
