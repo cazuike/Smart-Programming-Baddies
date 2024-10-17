9f9de0f5ba819d1badffe76ff19eff8a95de1444
Bryan Granda <bryan.a.granda@gmail.com>
2024-10-16 21:59:51 -0400

    Chore: Fixed all the stylechecking errors we had. Also, added the RouteController and debugged the App class to be able to run the Spring App.

--- serviceSPB/src/main/java/com/smartprogrammingbaddies/App.java
+++ serviceSPB/src/main/java/com/smartprogrammingbaddies/App.java
@@ -1,10 +1,36 @@
 package com.smartprogrammingbaddies;
 
+import jakarta.annotation.PreDestroy;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 /**
- * Class contains all the startup logic for the application.
+ * Class contains all the startup logic for the application.\
+ *
+ * @param args A {@code String[]} of any potential runtime arguments
  */
-public class App {
+@SpringBootApplication
+public class App implements CommandLineRunner {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    SpringApplication.run(App.class, args);
+  }
+
+  /**
+   * This function is required for the App to run.
+   *
+   * @param args A {@code String[]} of any potential runtime arguments
+   */
+  public void run(String[] args) {
+    System.out.println("World, hello!");
+  }
+
+  /**
+   * This contains all the overheading teardown logic.
+   */
+  @PreDestroy
+  public void onTermination() {
+    System.out.println("Je suis fini!");
   }
 }
