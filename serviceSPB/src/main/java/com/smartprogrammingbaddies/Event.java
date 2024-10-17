add7daa268cce32b1fa7f95cd281c2461bf94c1f
Evelyn Martinez <101659674+eevelxnn@users.noreply.github.com>
2024-10-17 18:24:37 -0400

    Implemented Event Class

--- serviceSPB/src/main/java/com/smartprogrammingbaddies/Event.java
+++ serviceSPB/src/main/java/com/smartprogrammingbaddies/Event.java
@@ -1,16 +1,105 @@
 package com.smartprogrammingbaddies;
 
 import java.io.Serializable;
+import java.util.Map;
 
+/**
+* The Event class represents an event organized by a StorageCenter, including its name, description, 
+* date, time, location, and the list of volunteers.
+*/
 public class Event implements Serializable {
 
-  public Event(String name, String description, String date, String time, String location, String organizaer){
+
+  public Event(String name, String description, String date, String time, String location, StorageCenter organizer, Map<String, Volunteer> listOfVolunteers){
     this.name = name;
     this.description = description;
     this.date = date;
     this.time = time;
     this.location = location;
-    this.organizer = organizaer;
+    this.organizer = organizer;
+    this.listOfVolunteers = listOfVolunteers;
+  }
+  
+
+  public String getName() {
+    return name;
+  }
+
+  public void updateName(String newName) {
+    this.name = newName;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void updateDescription(String newDescription) {
+    this.description = newDescription;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void updateDate(String newDate) {
+    this.date = newDate;
+  }
+  public String getTime() {
+    return time;
+  }
+
+  public void updateTime(String newTime) {
+    this.time = newTime;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void updateLocation(String newLocation) {
+    this.location = newLocation;
+  }
+
+  public StorageCenter getOrganizer() {
+    return organizer;
+  }
+
+  public Map<String, Volunteer> getListOfVolunteers() {
+    return listOfVolunteers;
+  }
+
+  public void addVolunteer(Volunteer volunteer){
+    listOfVolunteers.put(volunteer.getName(), volunteer);
+  }
+
+  public void removeVolunteer(String volunteerName) {
+    listOfVolunteers.remove(volunteerName);
+  }
+
+  public int getVolunteerCount() {
+    return this.listOfVolunteers.size();
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder eventDetails = new StringBuilder();
+    eventDetails.append("Event Name: ").append(name).append("\n")
+                  .append("Description: ").append(description).append("\n")
+                  .append("Date: ").append(date).append("\n")
+                  .append("Time: ").append(time).append("\n")
+                  .append("Location: ").append(location).append("\n")
+                  .append("Organizer: ").append(organizer.getName()).append("\n");
+
+    if (!listOfVolunteers.isEmpty()) {
+      eventDetails.append("Volunteer Names: \n");
+      for (String volunteerName : listOfVolunteers.keySet()) {
+          eventDetails.append("- ").append(volunteerName).append("\n");
+      }
+    } else {
+      eventDetails.append("No volunteers signed up yet.\n");
+    }      
+
+    return eventDetails.toString();
   }
 
   private String name;
@@ -18,5 +107,6 @@ public class Event implements Serializable {
   private String date;
   private String time;
   private String location;
-  private String organizer;
+  private StorageCenter organizer;
+  Map<String, Volunteer> listOfVolunteers;
 }
