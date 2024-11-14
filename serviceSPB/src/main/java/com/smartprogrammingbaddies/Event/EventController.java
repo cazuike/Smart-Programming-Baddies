219f35933226fa78571a8336c362e9a30e70e272
Bryan Granda <bryan.a.granda@gmail.com>
2024-11-14 18:31:05 -0500

    Added the pmd command to the CI workflow. Also debugged the EventController file

--- serviceSPB/src/main/java/com/smartprogrammingbaddies/Event/EventController.java
+++ serviceSPB/src/main/java/com/smartprogrammingbaddies/Event/EventController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
  * This class contains the EventController class.
  */
